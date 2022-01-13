67f28f7d346c2feeb6de107e1c08bc9b0407cf20
noob3-3 <xiaoshan3920@gmail.com>
2022-01-13 14:50:59 +0800

    添加cookie获取,cookie添加

---
+++ my_cookie.py
@@ -0,0 +1,38 @@
+# 获取cookies 到本地
+def get_cookies(url,driver):
+    driver.get(url)
+    time.sleep(20)  # 留时间进行扫码
+    Cookies = driver.get_cookies()  # 获取list的cookies
+    jsCookies = json.dumps(Cookies)  # 转换成字符串保存
+    with open('cookies.txt', 'w') as f:
+        f.write(jsCookies)
+    print('cookies已重新写入！')
+
+
+# 读取本地的cookies
+def read_cookies():
+    with open('cookies.txt', 'r', encoding='utf8') as f:
+        Cookies = json.loads(f.read())
+    cookies = []
+    for cookie in Cookies:
+        cookie_dict = {
+            'domain': '.weibo.com',
+            'name': cookie.get('name'),
+            'value': cookie.get('value'),
+            'expires': '',
+            'path': '/',
+            'httpOnly': False,
+            'HostOnly': False,
+            'Secure': False
+        }
+        cookies.append(cookie_dict)
+    return cookies
+
+def set_cookie(driver):
+    cookies = read_cookies()
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+    time.sleep(3)
+    driver.refresh()  # 刷新网页
+
+    return driver
